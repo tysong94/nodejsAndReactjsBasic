1a2f65a42d30ced985273592765b5cd0cfd15cc8
tysong94 <tysong94@oniontech.com>
2020-08-11 19:41:14 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://tysong94:wptkwkd94$@boilerplate.tataf.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => res.send('Hello World! 안녕하세요!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
